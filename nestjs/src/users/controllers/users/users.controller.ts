61d2c1ba5ecff5cdabb5b64b72c5daf4c9892e19
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2024-04-03 21:19:27 -0300

    feat: criando rota para cadastrar usuarios com as validações necessárias conforme banco

--- nestjs/src/users/controllers/users/users.controller.ts
+++ nestjs/src/users/controllers/users/users.controller.ts
@@ -1,9 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Header, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
 export class UsersController {
     constructor(private userService: UsersService) {}
+
+    @Post()
+    @Header ('Content-Type', 'application/json')
+    async createUser(@Body() createUserDto:  CreateUserDto){
+        try {
+            const newUser = await this.userService.createUser(createUserDto);
+            return newUser
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.CONFLICT);
+        }
+    }
 
     //rotas de usuarios aqui
     //Ex: @Get() - lista todos os dados da tabela users
