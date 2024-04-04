4ea6c389a0733a04e04e6091a2c375994229e919
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2024-04-04 20:31:21 -0300

    feat: criando rotas para pegar todos os usuarios e pegar um usuario por id com todas as suas relações

--- nestjs/src/users/controllers/users/users.controller.ts
+++ nestjs/src/users/controllers/users/users.controller.ts
@@ -1,19 +1,39 @@
-import { Body, Controller, Header, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, Header, HttpException, HttpStatus, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
 import { UsersService } from 'src/users/services/users/users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private userService: UsersService) {}
+    constructor(private userService: UsersService) { }
 
     @Post()
-    @Header ('Content-Type', 'application/json')
-    async createUser(@Body() createUserDto:  CreateUserDto){
+    @Header('Content-Type', 'application/json')
+    async createUser(@Body() createUserDto: CreateUserDto) {
         try {
             const newUser = await this.userService.createUser(createUserDto);
             return newUser
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.CONFLICT);
+        }
+    }
+
+    @Get()
+    async getUsers() {
+        try {
+            const users = await this.userService.getUsers()
+            return users
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.CONFLICT)
+        }
+    }
+
+    @Get(':id') // pega o id da rota /users/{id}
+    async getUserById(@Param('id', ParseIntPipe) id: number) { // usa os id da rota (ParseIntPipe): garante que o id é um numero
+        try {
+            const user = await this.userService.getUserById(id)
+            return user
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.CONFLICT)
         }
     }
 
