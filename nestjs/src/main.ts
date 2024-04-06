44062a3b8afbb88b9ff602aabd6c0fbbc24cc6f8
KhovetS2 <isaque.elis.escola@gmail.com>
2024-04-05 22:02:41 -0300

    fix: adicionar cors no backend

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(5000);
   const userService = app.get(UsersService)
   await userService.create_admin()
