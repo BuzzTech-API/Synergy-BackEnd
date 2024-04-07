2aa1bbed8d6c9f8a9f21bb15560026b62299ddb3
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2024-04-07 10:07:26 -0300

    refactor: atualiza as tabelas do banco de dados para a versão 6 e arruma os dados do administrador

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './entities/users.entity';
 import { PhysicalRooms } from './entities/physicalrooms.enity';
-import { Members } from './entities/members.entity';
+import { Participate } from './entities/participate.entity';
 import { Meetings } from './entities/meetings.entity';
 import { Guests } from './entities/guests.entity';
 import { Reservations } from './entities/reservations.entity';
-import { Reserveds } from './entities/reserveds.entity';
 import { VirtualRooms } from './entities/virtualrooms.entity';
 import { UsersModule } from './models/users/users.module';
 import { VirtualroomsModule } from './models/virtualrooms/virtualrooms.module';
@@ -16,6 +15,7 @@ import { ReservationsModule } from './models/reservations/reservations.module';
 import { AuthModule } from './auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthInterceptor } from './auth/auth.interceptor';
+import { Presence } from './entities/presence.entity';
 
 
 @Module({
@@ -27,7 +27,7 @@ import { AuthInterceptor } from './auth/auth.interceptor';
       username: 'Syatt',
       password: 'Senha123#',
       database: 'api',
-      entities: [Users, PhysicalRooms, Members, Meetings, Guests, Reservations, Reserveds, VirtualRooms],
+      entities: [Users, PhysicalRooms, Participate, Meetings, Guests, Reservations, VirtualRooms, Presence],
       synchronize: true,
       autoLoadEntities: true,
     }),
---
+++ nestjs/src/entities/presence.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Meetings } from './meetings.entity';
+import { Guests } from './guests.entity';
+
+@Entity({ name: 'Presencia' })
+export class Presence {
+    @PrimaryColumn({ name: 'guest_id' })
+    guest_id: number;
+
+    @PrimaryColumn({ name: 'meeting_id' })
+    meeting_id: number;
+
+    @Column({ type: "varchar", length: 60 })
+    guest_status: string;
+
+    // relações -------------------------------------------------
+
+    @ManyToOne(() => Guests, guest => guest.presence)
+    @JoinColumn({ name: 'guest_id' })
+    guest: Guests;
+
+    @ManyToOne(() => Meetings, meetings => meetings.participate)
+    @JoinColumn({ name: 'meeting_id' })
+    meetings: Meetings;
+
+}
