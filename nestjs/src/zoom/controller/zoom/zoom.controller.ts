4b87de3c4a79f33b155d58b7b75d13cf50061b4f
KhovetS2 <isaque.elis.escola@gmail.com>
2024-05-04 20:05:18 -0300

    fix: Arruma a rota do zoom

--- nestjs/src/zoom/controller/zoom/zoom.controller.ts
+++ nestjs/src/zoom/controller/zoom/zoom.controller.ts
@@ -1,32 +1,40 @@
-import { Controller, Get, Query, HttpException, HttpStatus, Res } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  HttpException,
+  HttpStatus,
+  Post,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
 import { ZoomService } from 'src/zoom/service/zoom/zoom.service';
 @Controller('zoom')
 export class ZoomController {
+  constructor(private zoomService: ZoomService) { }
 
-    constructor(private zoomService: ZoomService) {}
-
-
-
-    @Get('/token')
-    async getToken(code: string) {
-        try {
-          this.zoomService.getToken(code)
-        } catch (error) {
-            console.error('Error', error);
-            throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-
-    @Get('/refreshToken')
-    async refreshToken(@Query('refreshToken') refreshToken: string) {
-      try {
-        const response = await this.zoomService.refreshZoomToken(refreshToken);
-        return response;
-      } catch (error) {
-        console.error('Error', error);
-        throw new HttpException('Error refreshing token', HttpStatus.INTERNAL_SERVER_ERROR);
-      }
+  @HttpCode(200)
+  @Post('/token')
+  async getToken(@Body() code: string) {
+    try {
+      return this.zoomService.getToken(code);
+    } catch (error) {
+      console.error('Error', error);
+      throw new HttpException('Error', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
+  @Get('/refreshToken')
+  async refreshToken(@Query('refreshToken') refreshToken: string) {
+    try {
+      const response = await this.zoomService.refreshZoomToken(refreshToken);
+      return response;
+    } catch (error) {
+      console.error('Error', error);
+      throw new HttpException(
+        'Error refreshing token',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
