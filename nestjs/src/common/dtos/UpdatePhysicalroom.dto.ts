eedd5257cfa0ccec6fcc03997871c48d9b7ffae6
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2024-05-17 16:56:14 -0300

    feat: adiciona funções para atualizar e deletar salas físicas

---
+++ nestjs/src/common/dtos/UpdatePhysicalroom.dto.ts
@@ -0,0 +1,25 @@
+import { IsNotEmpty, IsString, Max, MaxLength, Min } from "class-validator"
+
+export class UpdatePhysicalroomDto{
+    @IsNotEmpty()
+    @Min(1)
+    physical_room_id: number
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(80)
+    physical_room_name: string
+
+    @IsNotEmpty()
+    @Min(1)
+    physical_room_vacancies: number
+
+    @IsString()
+    @IsNotEmpty()
+    physical_room_address: string
+
+    @IsNotEmpty()
+    @Min(1)
+    @Max(3)
+    physical_room_permission_level: number
+}
